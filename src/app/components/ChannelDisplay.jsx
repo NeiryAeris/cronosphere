fbaeacd046cd26714863fcc6991fad733b94caf8
NeiryAeris <Kukorysenpai3506@gmail.com>
2025-02-10 23:08:37 +0700

    adjustment

--- src/app/components/ChannelDisplay.jsx
+++ src/app/components/ChannelDisplay.jsx
@@ -1,18 +1,27 @@
+import Link from "next/link";
 import { artworks } from "../assets/data";
 import Image from "next/image";
 
-export default function ChannelDisplay({ selectedChannel , className }) {
+export default function ChannelDisplay({ selectedChannel, className }) {
   if (!selectedChannel) return <h1 className={`${className}`}>All Channels</h1>;
   return (
     <div className={`${className}`}>
-      <h1 className={`${className}`}>{selectedChannel}</h1>
-      <div className="w-auto h-auto grid-flow-col grid-cols-10">
+      <h1 className={`font-bold text-2xl`}>{selectedChannel}</h1>
+      <div className="w-auto h-auto grid grid-cols-7 gap-y-3">
         {artworks
-          .filter((artwork) => artwork.channel === selectedChannel)
+          .filter((artwork) => artwork.channelTitle === selectedChannel)
           .map((artwork, index) => (
-            <div key={index} className="w-20 h-20">
-              <Image src={artwork.img} alt="Artwork" className="w-20 h-20" />
+            <Link key={artwork.id} href={`/artwork/${artwork.id}`}>
+            <div key={index} className="max-w-[250px] max-h-[250px] w-[250px] h-[250px]">
+              <Image
+                src={artwork.img}
+                alt={artwork.title}
+                className="object-cover overflow-hidden w-full h-full rounded-sm"
+                width={250}
+                height={250}
+              />
             </div>
+            </Link>
           ))}
       </div>
     </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,14 @@
 "use client";
-import Navbar from "../components/Navbar";
-import BannerCarousel from "../components/BannerCarousel";
-import ChannelRibbon from "../components/ChannelRibbon";
-import ChannelDisplay from "../components/ChannelDisplay";
+import Navbar from "./components/Navbar";
+import BannerCarousel from "./components/BannerCarousel";
+import ChannelRibbon from "./components/ChannelRibbon";
+import ChannelDisplay from "./components/ChannelDisplay";
 import { useState } from "react";
 
 export default function HallPage() {
   const [selectedChannel, setSelectedChannel] = useState(null);
   return (
     <div>
-      <Navbar />
       <BannerCarousel />
 
       <ChannelRibbon onSelectChannel={setSelectedChannel}/>
