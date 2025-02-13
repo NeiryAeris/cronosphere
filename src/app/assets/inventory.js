671554eba854f357831839c3ba754cb3bcb4d941
NeiryAeris <Kukorysenpai3506@gmail.com>
2025-02-13 21:24:48 +0700

    prepare for dummy tags

--- src/app/assets/inventory.js
+++ src/app/assets/inventory.js
@@ -2,10 +2,12 @@ import dummy from "./placeholder.jpg";
 import banner from "./banner.jpg";
 import channel from "./channel.jpg";
 import wide_banner from "./wide_banner.jpg";
+import sub_banner from "./sub_banner.jpg";
 
 export const items = {
   dummy,
   banner,
   channel,
   wide_banner,
+  sub_banner,
 };
--- src/app/components/ShopNav.jsx
+++ src/app/components/ShopNav.jsx
@@ -7,6 +7,7 @@ export default function ShopNav() {
   return (
     <div>
       <div className="flex w-auto h-auto m-2 gap-2">
+        {/*1. this section will decided weather section 2 appear or not*/}
         <div onClick={() => {}}>
           <Image src={items.wide_banner} alt="logo" className="h-24 object-cover" />
         </div>
@@ -14,7 +15,32 @@ export default function ShopNav() {
           <Image src={items.wide_banner} alt="logo" className="h-24 object-cover" />
         </div>
       </div>
-      <div></div>
+      <div>
+        {/*2. this section will appear if section 1 is clicked*/}
+        <div className="flex w-auto h-auto m-2 gap-2">
+          <div onClick={() => {}}>
+            <Image src={items.sub_banner} alt="logo" className="h-24 object-cover" />{" "}
+          </div>
+          <div>
+            <Image src={items.sub_banner} alt="logo" className="h-24 object-cover" />{" "}
+          </div>
+          <div onClick={() => {}}>
+            <Image src={items.sub_banner} alt="logo" className="h-24 object-cover" />{" "}
+          </div>
+          <div onClick={() => {}}>
+            <Image src={items.sub_banner} alt="logo" className="h-24 object-cover" />{" "}
+          </div>
+          <div onClick={() => {}}>
+            <Image src={items.sub_banner} alt="logo" className="h-24 object-cover" />{" "}
+          </div>
+          <div onClick={() => {}}>
+            <Image src={items.sub_banner} alt="logo" className="h-24 object-cover" />{" "}
+          </div>
+          <div onClick={() => {}}>
+            <Image src={items.sub_banner} alt="logo" className="h-24 object-cover" />{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
