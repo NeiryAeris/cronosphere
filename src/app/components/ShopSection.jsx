2170a53eee48013cb3f322d11b723a24ee8204d6
NeiryAeris <Kukorysenpai3506@gmail.com>
2025-02-15 22:17:53 +0700

    Update ShopSection.jsx

--- src/app/components/ShopSection.jsx
+++ src/app/components/ShopSection.jsx
@@ -2,16 +2,22 @@ import { marketItems } from "../assets/data2";
 import Image from "next/image";
 import Link from "next/link";
 
+import { artists } from "../assets/data";
+
 export default function ShopSection() {
   return (
-    <div>
-      <div className="grid grid-cols-5 gap-4">
+    <div className="m-4">
+      <div className="grid grid-cols-5 gap-4 hover:">
         {marketItems.map((item) => (
           <Link key={item.itemID} href={`/shop/${item.itemID}`}>
-            <div key={item.itemID}>
-              <Image src={item.itemImage} alt={item.itemName} width={250} height={250} />
-              <h1>{item.itemName}</h1>
-              <p>{item.itemPrice}</p>
+            <div key={item.itemID} className="max-w-[350px] w-[350px] hover:bg-slate-50">
+              <Image src={item.itemImage} alt={item.itemName} width={350} height={350} />
+              <h1 className="m-2">{item.itemName}</h1>
+              <div className="m-2 flex">
+                <Image src={artists.img} />
+                <p>by {item.itemAuthorID}</p>
+                <p className="ml-auto mr-2">{item.itemPrice}$</p>
+              </div>
             </div>
           </Link>
         ))}
