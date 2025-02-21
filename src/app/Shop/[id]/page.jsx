1cb88342677544ae3e88d646c5d7c8dfd15ef74c
NeiryAeris <Kukorysenpai3506@gmail.com>
2025-02-21 23:42:53 +0700

    Update page.jsx

--- src/app/Shop/[id]/page.jsx
+++ src/app/Shop/[id]/page.jsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import avatar from "./alting.jpg";
-import { ShoppingBasket } from "lucide-react";
+import { Heart, ShoppingBasket } from "lucide-react";
 
 export default async function MarketItemDetail({ params }) {
   const { id } = await params;
 
   return (
-    <div className="px-10 flex box-border">
-      <div className="w-screen bg-slate-600 mr-10">
+    <div className="px-10 box-border">
+      <div className="w-full bg-slate-600 mr-10">
         <div>
           <p>Marketplace / Game Assets / Industrial Smart Materials</p>
         </div>
@@ -20,64 +20,84 @@ export default async function MarketItemDetail({ params }) {
             </Link>
             <span className="mr-2 text-gray-500">by</span>
             <p>author name</p>
-            <span></span>
-            <Link href={"/"} className="ml-2 cursor-pointer underline text-blue-500 hover:text-white">
+            <span className="mx-2 w-1 h-1 bg-white rounded-full inline-block"></span>
+            <Link href={"/"} className=" cursor-pointer underline text-blue-500 hover:text-white">
               Seller Infomation
             </Link>
           </div>
         </div>
       </div>
-      <div className="w-[550px] bg-slate-400">
+
+      <div className="flex w-full bg-slate-400">
+        {/* the section below is marketplace item descriptions */}
+        <div className="w-screen"></div>
         {/* later on, the section below will display the bundles that the item have */}
-        <div className="mb-5 bg-cyan-600 mx-4">
-          <div>
-            <h1>
-              bundle title <span>Free</span>
-            </h1>
+        <div className="w-[750px]">
+          <div className="mb-5 bg-cyan-600 mx-4 p-4 rounded-md border box-border">
+            <div>
+              <h1>
+                bundle title <span>Free</span>
+              </h1>
+            </div>
+            <div>
+              <p className="text-gray-500">
+                License: <span>Standart License</span>
+              </p>
+              <p>License type</p>
+            </div>
+            <div className="flex">
+              <div className="w-full flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md py-3">
+                <ShoppingBasket size={20} />
+                <p className="pl-2">Add To Cart</p>
+              </div>
+              <div className="flex justify-center items-center p-2 my-1 border rounded-md hover:bg-slate-500">
+                <Heart size={30} />
+              </div>
+            </div>
           </div>
-          <div>
-            <p className="text-gray-500">
-              License: <span>Standart License</span>
-            </p>
-            <p>License type</p>
+          <div className="mb-5 bg-cyan-600 mx-4 p-4 rounded-md border box-border">
+            <div>
+              <h1>
+                bundle title <span>Free</span>
+              </h1>
+            </div>
+            <div>
+              <p className="text-gray-500">
+                License: <span>Standart License</span>
+              </p>
+              <p>License type</p>
+            </div>
+            <div className="flex">
+              <div className="w-full flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md py-3">
+                <ShoppingBasket size={20} />
+                <p className="pl-2">Add To Cart</p>
+              </div>
+              <div className="flex justify-center items-center p-2 my-1 border rounded-md hover:bg-slate-500">
+                <Heart size={30} />
+              </div>
+            </div>
           </div>
-          <div className="flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md">
-            <ShoppingBasket size={20} />
-            <p className="pl-2">Add To Cart</p>
-          </div>
-        </div>
-        <div className="mb-5 bg-cyan-600 mx-4">
-          <div>
-            <h1>
-              bundle title <span>Free</span>
-            </h1>
-          </div>
-          <div>
-            <p className="text-gray-500">
-              License: <span>Standart License</span>
-            </p>
-            <p>License type</p>
-          </div>
-          <div className="flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md">
-            <ShoppingBasket size={20} />
-            <p className="pl-2">Add To Cart</p>
-          </div>
-        </div>
-        <div className="mb-5 bg-cyan-600 mx-4">
-          <div>
-            <h1>
-              bundle title <span>Free</span>
-            </h1>
-          </div>
-          <div>
-            <p className="text-gray-500">
-              License: <span>Standart License</span>
-            </p>
-            <p>License type</p>
-          </div>
-          <div className="flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md">
-            <ShoppingBasket size={20} />
-            <p className="pl-2">Add To Cart</p>
+          <div className="mb-5 bg-cyan-600 mx-4 p-4 rounded-md border box-border">
+            <div>
+              <h1>
+                bundle title <span>Free</span>
+              </h1>
+            </div>
+            <div>
+              <p className="text-gray-500">
+                License: <span>Standart License</span>
+              </p>
+              <p>License type</p>
+            </div>
+            <div className="flex">
+              <div className="w-full flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md py-3">
+                <ShoppingBasket size={20} />
+                <p className="pl-2">Add To Cart</p>
+              </div>
+              <div className="flex justify-center items-center p-2 my-1 border rounded-md hover:bg-slate-500">
+                <Heart size={30} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
