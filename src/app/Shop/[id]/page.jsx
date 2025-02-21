11c706ff3217f7fe5ffe8bdb070bcf79fd283e04
NeiryAeris <Kukorysenpai3506@gmail.com>
2025-02-21 19:38:40 +0700

    Update page.jsx

--- src/app/Shop/[id]/page.jsx
+++ src/app/Shop/[id]/page.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import avatar from "./alting.jpg";
+import { ShoppingBasket } from "lucide-react";
 
 export default async function MarketItemDetail({ params }) {
   const { id } = await params;
 
   return (
-    <div className="px-10 flex">
-      <div>
+    <div className="px-10 flex box-border">
+      <div className="w-screen bg-slate-600 mr-10">
         <div>
           <p>Marketplace / Game Assets / Industrial Smart Materials</p>
         </div>
@@ -17,7 +18,7 @@ export default async function MarketItemDetail({ params }) {
             <Link href={"/"}>
               <Image src={avatar} alt="author avatar" width={20} height={20} className="rounded-lg mr-2" />
             </Link>
-            <span className="mr-2 text-gray-600">by</span>
+            <span className="mr-2 text-gray-500">by</span>
             <p>author name</p>
             <span></span>
             <Link href={"/"} className="ml-2 cursor-pointer underline text-blue-500 hover:text-white">
@@ -26,7 +27,60 @@ export default async function MarketItemDetail({ params }) {
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="w-[550px] bg-slate-400">
+        {/* later on, the section below will display the bundles that the item have */}
+        <div className="mb-5 bg-cyan-600 mx-4">
+          <div>
+            <h1>
+              bundle title <span>Free</span>
+            </h1>
+          </div>
+          <div>
+            <p className="text-gray-500">
+              License: <span>Standart License</span>
+            </p>
+            <p>License type</p>
+          </div>
+          <div className="flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md">
+            <ShoppingBasket size={20} />
+            <p className="pl-2">Add To Cart</p>
+          </div>
+        </div>
+        <div className="mb-5 bg-cyan-600 mx-4">
+          <div>
+            <h1>
+              bundle title <span>Free</span>
+            </h1>
+          </div>
+          <div>
+            <p className="text-gray-500">
+              License: <span>Standart License</span>
+            </p>
+            <p>License type</p>
+          </div>
+          <div className="flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md">
+            <ShoppingBasket size={20} />
+            <p className="pl-2">Add To Cart</p>
+          </div>
+        </div>
+        <div className="mb-5 bg-cyan-600 mx-4">
+          <div>
+            <h1>
+              bundle title <span>Free</span>
+            </h1>
+          </div>
+          <div>
+            <p className="text-gray-500">
+              License: <span>Standart License</span>
+            </p>
+            <p>License type</p>
+          </div>
+          <div className="flex justify-center text-center hover:bg-slate-400 mx-2 my-1 bg-yellow-500 rounded-md">
+            <ShoppingBasket size={20} />
+            <p className="pl-2">Add To Cart</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
