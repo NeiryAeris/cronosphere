855a3ca3c1155664f2270ebb0f32d52e7e8538fb
NeiryAeris <Kukorysenpai3506@gmail.com>
2025-02-21 16:20:24 +0700

    do nothing tho

--- src/app/Shop/[id]/page.jsx
+++ src/app/Shop/[id]/page.jsx
@@ -1,7 +1,32 @@
-export default function MarketItemDetail() {
-    return (
+import Image from "next/image";
+import Link from "next/link";
+import avatar from "./alting.jpg";
+
+export default async function MarketItemDetail({ params }) {
+  const { id } = await params;
+
+  return (
+    <div className="px-10 flex">
+      <div>
         <div>
-            Enter
+          <p>Marketplace / Game Assets / Industrial Smart Materials</p>
         </div>
-    );
+        <div>
+          <h1>Marketplace Item Title</h1>
+          <div className="flex items-center">
+            <Link href={"/"}>
+              <Image src={avatar} alt="author avatar" width={20} height={20} className="rounded-lg mr-2" />
+            </Link>
+            <span className="mr-2 text-gray-600">by</span>
+            <p>author name</p>
+            <span></span>
+            <Link href={"/"} className="ml-2 cursor-pointer underline text-blue-500 hover:text-white">
+              Seller Infomation
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 }
--- src/app/components/ShopSection.jsx
+++ src/app/components/ShopSection.jsx
@@ -10,7 +10,7 @@ export default function ShopSection() {
     <div className="m-4">
       <div className="grid grid-cols-5 gap-4 hover:">
         {marketItems.map((item) => (
-          <Link key={item.itemID} href={`/shop/${item.itemID}`}>
+          <Link key={item.itemID} href={`/Shop/${item.itemID}`}>
             <div key={item.itemID} className="max-w-[350px] w-[350px] hover:bg-slate-800 pb-2">
               <Image src={item.itemImage} alt={item.itemName} width={350} height={350} />
               <h1 className="m-2">{item.itemName}</h1>
